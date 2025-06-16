0c7568a618ae05342bc4a19625643bd27d1e3c62
HDB23 <hetbordiwala2006@gmail.com>
2025-06-17 00:20:46 +0530

    first commit

---
+++ src/Components/Sections/Projects.jsx
@@ -0,0 +1,132 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-5xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            Featured Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:-border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">To Do App</h3>
+              <p className="textgray-400 mb-4">
+                React JS-powered To-Do app offers a sleek and intuitive way to
+                manage tasks effortlessly. With smooth performance and a clean
+                design, it makes productivity both simple and visually
+                appealing!
+              </p>
+              <div>
+                {["React", "JavaScript", "TailwindCSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  target="_blank"
+                  href="https://my-to-do-app-snowy.vercel.app/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:-border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Simple Landing Page</h3>
+              <p className="textgray-400 mb-4">
+                Simple Landing Page presents a sleek and modern aesthetic,
+                delivering a clean, engaging introduction to content. With a
+                responsive design and smooth layout, it ensures a polished user
+                experience across devices!
+              </p>
+              <div>
+                {["HTML", "CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  target="_blank"
+                  href="https://hdb23.github.io/simple_landing_page/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:-border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Naturize</h3>
+              <p className="textgray-400 mb-4">
+                Naturize delivers a refreshing and nature-inspired digital
+                experience, blending aesthetic elegance with a smooth,
+                user-friendly interface. Its clean design and serene theme make
+                for an engaging and visually calming web presence!
+              </p>
+              <div>
+                {["HTML", "CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  target="_blank"
+                  href="https://hdb23.github.io/Naturize/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:-border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Portfolio</h3>
+              <p className="textgray-400 mb-4">
+                My portfolio website is a reflection of my journey, blending
+                creativity with technical expertise. It's evolving into a space
+                that showcases my skills in a visually engaging and seamless
+                way!
+              </p>
+              <div>
+                {["React", "JavaScript", "TailwindCSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  target="_blank"
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
